8ff20dd0a4b58c18a2bd9273461d5714e2e0c604
Lu WANG <luwang@MBP-de-Lu.lan>
2019-12-11 14:32:13 +0800

    Initialization

---
+++ app/src/main/java/fr/epita/android/helloclick/MainActivity.kt
@@ -0,0 +1,12 @@
+package fr.epita.android.helloclick
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
